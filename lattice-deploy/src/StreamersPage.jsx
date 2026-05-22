33f2ad139454747f64671b588b1c2e6c224296ac
happygamer001 <jessica@chipperfield.ag>
2026-05-21 20:12:11 -0600

    Update StreamersPage.jsx

--- lattice-deploy/src/StreamersPage.jsx
+++ lattice-deploy/src/StreamersPage.jsx
@@ -295,7 +295,7 @@ export default function StreamersPage() {
         </main>
       </div>
 
-      <SiteFooter />
+      <SiteFooter mode="fixed" />
 
       {signupOpen && (
         <SignupModal
